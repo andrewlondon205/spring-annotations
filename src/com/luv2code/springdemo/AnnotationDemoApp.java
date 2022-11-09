a25a7b286d479bf3a4704c075121fece349fbbb4
Ramai Alejandria <vyasadev931@gmail.com>
2022-11-09 18:59:09 +0100

    Initial commit

---
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+    public static void main(String[] args) {
+        // read spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // get the bean from spring container
+        
+        // call a method on the bean
+        // close the context
+    }
+}
