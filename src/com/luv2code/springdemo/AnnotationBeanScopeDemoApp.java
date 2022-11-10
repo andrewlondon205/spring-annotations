42545fd8807cc8e9a281c461231a8c62fd597724
AlejandriaRamai <vyasadev931@gmail.com>
2022-11-10 17:02:41 +0100

    further implementations

--- src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -2,7 +2,6 @@ package com.luv2code.springdemo;
 
 public class AnnotationBeanScopeDemoApp {
     public static void main(String[] args) {
-
         //load spring config file
         // retrieve bean from spring container
     }
