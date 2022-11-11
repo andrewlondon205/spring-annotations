0c825e7d5b40cd09ce49c9c458757bf454bd7266
Ramai Alejandria <vyasadev931@gmail.com>
2022-11-11 15:21:37 +0100

    New class and interface implementations

--- src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -1,8 +1,26 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationBeanScopeDemoApp {
+
     public static void main(String[] args) {
         //load spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         // retrieve bean from spring container
+        Coach theCoach = context.getBean("tennisCoach",Coach.class);
+        Coach alphaCoach = context.getBean("tennisCoach",Coach.class);
+
+        //check if they are the same object
+        boolean result = (theCoach == alphaCoach);
+
+        // print out the results
+   /*     System.out.println("Pointing to the same object: " + result);
+        System.out.println("Memory location: " + theCoach);
+        System.out.println("Memory location: " + alphaCoach);*/
+
+        //close the context
+        context.close();
     }
+
 }
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -2,8 +2,11 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.IOException;
 
 @Component
@@ -16,6 +19,17 @@ public class TennisCoach implements Coach{
     //define a default constructor
     public TennisCoach () {
         System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    //define my init method
+    @PostConstruct
+    public void doMyStartupStuff () {
+        System.out.println(">>TennisCoach: inside of doMyStartupStuff()");
+    }
+    // define my destroy code
+    @PreDestroy
+    public void doMyCleanupStuff () {
+        System.out.println(">>TennisCoach: inside of doMyCleanupStuff()");
     }
 
 /*
